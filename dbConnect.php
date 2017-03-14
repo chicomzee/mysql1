15d95b2061666d2ae9c8e81aa7a7e19e89d55a6f
fildaokafor <1611529@rgu.ac.uk>
2017-03-14 16:56:43 +0000

    7t7

--- dbConnect.php
+++ dbConnect.php
@@ -16,7 +16,7 @@ $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-echo "connected";
+//echo "connected";
 if (!$link) {
 echo "Error: Unable to connect to MySQL." . PHP_EOL;
 echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
@@ -27,5 +27,5 @@ exit;
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close($link);
+//mysqli_close($link);
 ?>
