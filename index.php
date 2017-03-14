7d19d820e1a856137d6ca79c72cc24860197cfc2
fildaokafor <1611529@rgu.ac.uk>
2017-03-14 14:07:15 +0000

    7t7

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: User2
- * Date: 07/03/2017
- * Time: 16:51
+ * create a SQL query as a string
+ * execute the SQL query
+ * iterate over $result object one $row at a time
+ * use fetch_array() to return an associative array
+ * process the $row
+
  */
+
+$sql query =SELECT *FROM superheros WHERE super
+?>
